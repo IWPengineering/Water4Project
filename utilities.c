01063de9510c54afbe6a751da7ac4ba7b6c1b626
KenKok <kk1311@messiah.edu>
2016-03-22 13:24:55 -0400

    Added reset bit clear

--- utilities.c
+++ utilities.c
@@ -42,56 +42,68 @@ void resetCheckRemedy(void)
 {
     if(_TRAPR)
     {
-        
+        // Trap Reset Conflict has occured
+        _TRAPR = 0;
     }
     
     if(_IOPUWR)
     {
-        
+        // Illegal Opcode, illegal address mode, or
+        //  W reg used as address pointer caused a reset
+        _IOPUWR = 0;
     }
 
     if(_DPSLP)
     {
-        
+        // Woke up from Deep Sleep Mode
+        _DPSLP = 0;
     }
     
     if(RCONbits.CM)
     {
-        
+        // Configuration word mismatch occurred
+        RCONbits.CM = 0;
     }
     
     if(_EXTR)
     {
-        
+        // External Reset (MCLR Pin) has occurred
+        _EXTR = 0;
     }
     
     if(_SWR)
     {
-        
+        // Software Reset has occurred
+        _SWR = 0;
     }
     
     if(_WDTO)
     {
-        
+        // Watchdog timeout reset has occurred
+        _WDTO = 0;
     }
     
     if(_SLEEP)
     {
-        
+        // Woke up from sleep mode
+        _SLEEP = 0;
     }
     
     if(_IDLE)
     {
-        
+        // Woke up from idle mode
+        _IDLE = 0;
     }
     
     if(_BOR)
     {
-        
+        // Brownout caused reset
+        _BOR = 0;
     }
     
     if(_POR)
     {
-        
+        // Power-up reset has occurred
+        _POR = 0;
     }
 }
